c65bf061a6d08c9859b2e409b4397f4014746528
destiny-dream <58924134+destiny-dream@users.noreply.github.com>
2021-01-15 19:47:00 +0800

    Update diy-part1.sh
    del rblibtorrent renew

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,5 +21,5 @@ sed -i 's/4.3.1/4.3.2/g' package/lean/qBittorrent/Makefile
 sed -i 's/4428ed710cfbcc48e75c4cdb014b803ea3f67311f55f4cba0d148987986bd6c8/37d49b70ba5319c3dc208f22a503671e21b1212b8099b8bfff1ca570fed32360/g' package/lean/qBittorrent/Makefile
 
 #rblibtorrent
-sed -i 's/1.2.11/2.0.2/g' package/lean/rblibtorrent/Makefile
-sed -i 's/471e772cb7038f1bf5f44c32a09eb42fbb80ee99/d5960e8c9f80f62126d723c6cc0522e7900c9c323f28994027eae3325fe03a3f/g' package/lean/rblibtorrent/Makefile
+#sed -i 's/1.2.11/2.0.2/g' package/lean/rblibtorrent/Makefile
+#sed -i 's/471e772cb7038f1bf5f44c32a09eb42fbb80ee99/d5960e8c9f80f62126d723c6cc0522e7900c9c323f28994027eae3325fe03a3f/g' package/lean/rblibtorrent/Makefile
